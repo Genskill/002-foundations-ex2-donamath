f71e7916f75fe5a699be8b27a5e66a978d86ec72
Dona Mathew <50779354+donamath@users.noreply.github.com>
2021-05-18 17:15:44 +0530

    Create pythagorean.c

---
+++ pythagorean.c
@@ -0,0 +1,45 @@
+./
+ side 1 1
+ side 2 2
+ side 3 3
+ No
+ 
+ #include<stdio.h>
+ #include<cs50.h>
+ 
+ int main(void){
+    int a,b,c,lhs,rhs;
+    a=get_int("side 1 ");
+    b=get_int("side 2 ");
+    c=get_int("side 3 ");
+    if((a>b) && (a>c))
+    {
+        lhs=(b*b)+(c*c);
+        rhs=a*a;
+        if(lhs==rhs)
+            printf("Yes");
+        else
+            printf("No");
+    }
+    else if((b>a) && (b>c))
+    {
+        lhs=(a*a)+(c*c);
+        rhs=b*b;
+        if(lhs==rhs)
+            printf("Yes");
+        else
+            printf("No");
+    }
+    else 
+    {
+        lhs=(a*a)+(b*b);
+        rhs=c*c;
+        if(lhs==rhs)
+            printf("Yes");
+        else
+            printf("No");
+    }
+    
+    
+    
+ }
